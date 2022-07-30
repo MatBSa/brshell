22e28edce857cdd3d23b31172dc57fb1699052a9
MatBSa <matheus.bsa@hotmail.com>
2022-07-30 15:06:36 -0300

    Implementation of the lexical scanner, parser and executor

--- main.c
+++ main.c
@@ -2,7 +2,12 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+
 #include "shell.h"
+#include "source.h"
+#include "parser.h"
+#include "node.h"
+#include "executor.h"
 
 int main(int argc, char **argv)
 {
@@ -25,7 +30,13 @@ int main(int argc, char **argv)
             free(cmd);
             break;
         }
-        printf("%s\n", cmd);
+
+        struct source_s src;
+        src.buffer = cmd;
+        src.bufsize = strlen(cmd);
+        src.curpos = INIT_SRC_POS;
+        parse_and_execute(&src);
+
         free(cmd);
     } while (1);
     exit(EXIT_SUCCESS);
@@ -85,4 +96,26 @@ char *read_cmd(void)
     }
 
     return ptr;
+}
+
+int parse_and_execute(struct source_s *src)
+{
+    skip_white_spaces(src);
+    struct token_s *tok = tokenize(src);
+    if (tok == &eof_token)
+    {
+        return 0;
+    }
+    while (tok && tok != &eof_token)
+    {
+        struct node_s *cmd = parse_simple_command(tok);
+        if (!cmd)
+        {
+            break;
+        }
+        do_simple_command(cmd);
+        free_node_tree(cmd);
+        tok = tokenize(src);
+    }
+    return 1;
 }
